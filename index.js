a9651d4b4289dad7347441ddc9e831b5c4a36803
Ricardo Ribeiro <kodii@kodii.eu>
2020-10-22 09:07:08 +0200

    refactoring code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const message = document.getElementById("message")
-const playerBetBox = document.getElementById('player-bet-box')
-const inputBet = document.getElementById('player-bet-box')
-const validateBetBtn = document.getElementById('validate-bet-btn')
+const playerBetBox = document.getElementById('playerBetBox')
+const inputBet = document.getElementById('inputBet')
+const validateBetBtn = document.getElementById('validateBetBtn')
+const totalBets = document.getElementsByClassName('totalBets')
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
@@ -10,6 +11,7 @@ const player1 = {
     player1Dice: document.getElementById("player1Dice"),
     palyer1MoneyBet: document.getElementById('palyer1MoneyBet'),
     player1Scoreboard: document.getElementById("player1Scoreboard"),
+    palyer1total: document.getElementsByClassName('palyer1total'),
     player1Turn: true
 }
 
@@ -18,6 +20,7 @@ const player2 = {
     player2Dice: document.getElementById("player2Dice"),
     palyer2MoneyBet: document.getElementById('palyer2MoneyBet'),
     player2Scoreboard: document.getElementById("player2Scoreboard"),
+    player2total: document.getElementsByClassName('player2total'),
     player2Turn: false
 }
 // console.log(player1.player1Dice)
@@ -31,61 +34,14 @@ rollBtn.addEventListener('click', function () {
         player2.player2Dice.classList.remove('active')
         player1.player1Dice.classList.add('active')
         message.textContent = `Player 1 turned ${randomNumber}`
-        double()
     } else {
         player2.player2Dice.textContent = randomNumber
         player1.player1Dice.classList.remove('active')
         player2.player2Dice.classList.add('active')
         message.textContent = `Player 2 turned ${randomNumber}`
-        double()
     }
     player1.player1Turn = !player1.player1Turn
     // console.log(player1.player1Dice)
     // console.log(player2.player2Dice)
 })
 
-function double() {
-
-    if (randomNumber >= 15 && randomNumber < 21) {
-        splitBtnBox.style.display = 'block'
-
-        if (player1.player1Turn) {
-
-            yesSplitBtn.addEventListener('click', function () {
-                console.log('splited')
-                player1.player1Dice.textContent = Math.floor(randomNumber / 2)
-                player1.player1SplitedDice.style.display = 'block'
-                player1.player1SplitedDice.classList.add('active')
-                player1.player1SplitedDice.textContent = player1.player1Dice.textContent
-            })
-
-            noSplitBtn.addEventListener('click', function () {
-                message.textContent = 'Player 2 its your turn'
-            })
-
-        } else if (player2.player2Turn) {
-
-            yesSplitBtn.addEventListener('click', function () {
-                console.log('splited')
-                player2.player2Dice.textContent = Math.floor(randomNumber / 2)
-                player2.player2SplitedDice.style.display = 'block'
-                player2.player2SplitedDice.classList.add('active')
-                player2.player2SplitedDice.textContent = player2.player2Dice.textContent
-            })
-
-            noSplitBtn.addEventListener('click', function () {
-                message.textContent = 'Player 1 its your turn'
-                splitBtnBox.style.display = 'none'
-            })
-
-        }
-
-        noSplitBtn.addEventListener('click', function () {
-            console.log('roll dice')
-        })
-    } else if (randomNumber === 21) {
-        console.log('You Win')
-    } else if (randomNumber > 21) {
-        console.log('You loose')
-    }
-}
