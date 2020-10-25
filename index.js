f2a907f2d01b3cdc3678aaa5c861b6119879f2b1
Ricardo Ribeiro <kodii@kodii.eu>
2020-10-25 17:31:45 +0100

    validate bet test

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const message = document.getElementById("message")
 const playerBetBox = document.getElementById('playerBetBox')
 const inputBet = document.getElementById('inputBet')
-const validateBetBtn = document.getElementById('validateBetBtn')
+const addBetBtn = document.getElementById('addBetBtn')
 const totalBets = document.querySelector('.totalBets')
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
+let player1Bet = 0
+let player2Bet = 0
+let player1Score = 0
+let player2Score = 0
 
 const player1 = {
-    player1Score: 0,
     player1Dice: document.getElementById("player1Dice"),
     palyer1MoneyBet: document.getElementById('palyer1MoneyBet'),
     player1Scoreboard: document.getElementById("player1Scoreboard"),
+    player1TotalBox: document.querySelector('.player1TotalBox'),
     player1Total: document.querySelector('.player1total'),
     player1Turn: true
 }
@@ -20,16 +24,15 @@ const player2 = {
     player2Dice: document.getElementById("player2Dice"),
     palyer2MoneyBet: document.getElementById('palyer2MoneyBet'),
     player2Scoreboard: document.getElementById("player2Scoreboard"),
+    player2TotalBox: document.querySelector('.player2TotalBox'),
     player2Total: document.querySelector('.player2total'),
     player2Turn: false
 }
 
 // add bets from players
-function addBet() {
-    let player1Bet = 0
-    let player2Bet = 0
 
-    validateBetBtn.addEventListener('click', function () {
+function addBet() {
+    addBetBtn.addEventListener('click', function () {
         if (player1.player1Turn) {
             player1Bet = parseInt(inputBet.value)
             player1.player1Total.textContent -= player1Bet
@@ -42,14 +45,31 @@ function addBet() {
             message.textContent = 'Player 1 insert your BET.'
             message.textContent = 'Roll the DICE'
             inputBet.disabled = true
-            validateBetBtn.disabled = true
+            addBetBtn.disabled = true
         }
         player1.player1Turn = !player1.player1Turn
         totalBets.textContent = player1Bet + player2Bet
-        playGame()
     })
 }
 addBet()
+
+// function validateBet() {
+//     console.log('RUN')
+//     if (player1Bet > player1.player1Total.textContent) {
+//         console.log('Sorry no funds available, try again.')
+//         message.textContent = 'Sorry no funds available, try again.'
+//         player1.player1TotalBox.style.border = '5px solid var(--color-5)'
+//         inputBet.value = ''
+//         player1Bet = 0
+//     }
+
+//     if (player2Bet > player2.player2Total.textContent) {
+//         message.textContent = 'Sorry no funds available.'
+//         inputBet.value = ''
+//         player2.player2TotalBox.style.border = '5px solid var(--color-5)'
+//     }
+// }
+
 
 let randomNumber = 0
 
@@ -59,11 +79,15 @@ function playGame() {
         randomNumber = Math.floor(Math.random() * 22) + 1
 
         if (player1.player1Turn) {
+            player1Score += randomNumber
+            player1.player1Scoreboard.textContent += player1Score
             player1.player1Dice.textContent = randomNumber
             player2.player2Dice.classList.remove('active')
             player1.player1Dice.classList.add('active')
             message.textContent = `Player 1 turned ${randomNumber}`
         } else {
+            player2Score += randomNumber
+            player2.player2Scoreboard.textContent += player2Score
             player2.player2Dice.textContent = randomNumber
             player1.player1Dice.classList.remove('active')
             player2.player2Dice.classList.add('active')
@@ -72,9 +96,18 @@ function playGame() {
         player1.player1Turn = !player1.player1Turn
     })
 }
+playGame()
 
 function gameCases() {
     switch (randomNumber) {
-        
+        case 1:
+            randomNumber > 21
+            message.textContent = 'You LOST!'
+            break;
+        case 2:
+            randomNumber === 21
+            message.textContent = 'You WIN!'
+            break;
+
     }
 }
